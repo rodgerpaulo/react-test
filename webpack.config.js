879be62c8cb9b2d4f7772146197f692902895d7d
Roger Ramos <rodgerpaulo@hotmail.com>
2018-03-29 19:31:06 -0300

    chore: adding html copy and clean plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanPlugin = require('webpack-clean-plugin')
 
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     entry: './src/index.ts',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist/'),
         filename: 'bundle.min.js'
     },
     resolve: {
         extensions: ['ts']
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+      new WebpackCleanPlugin({
+        on: 'emit',
+        path: ['./dist']
+      })
+    ],
     module: {
         rules: [
             { test: /\.ts$/, use: 'ts-loader' }
